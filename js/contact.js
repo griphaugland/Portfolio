3f3b5584afab8e1eb16cc372b66982dfa88f6317
griphaugland <hauglanden1@gmail.com>
2023-06-11 15:53:33 +0200

    Changed iframe size and positioning and changed button placement. Also added functional contact form

--- js/contact.js
+++ js/contact.js
@@ -1,39 +1,7 @@
-const nameInput = document.getElementById('name')
-const emailInput = document.getElementById('email')
-const messageInput = document.getElementById('message')
-
-const namelabel = document.querySelector('#namelabel')
-const emaillabel = document.querySelector('#emaillabel')
-const messagelabel = document.querySelector('#messagelabel')
-const submit = document.querySelector('#formButton');
-
-
-const form = document.querySelector("form"),
-statusTxt = form.querySelector("#overallFeedback");
-form.onsubmit = (e)=>{
-  e.preventDefault();
-  statusTxt.style.color = "lightgray";
-  statusTxt.style.display = "block";
-  statusTxt.innerText = "Sending your message...";
-  form.classList.add("disabled");
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "message.php", true);
-  xhr.onload = ()=>{
-    if(xhr.readyState == 4 && xhr.status == 200){
-      let response = xhr.response;
-      console.log(response)
-    if(response.indexOf('Email and password fields are required.') != -1 || response.indexOf('Enter a valid email address!') != -1 ||response.indexOf('We have some owls in the moss here. Please try again.' != -1)){
-        statusTxt.style.color = "#BE5252";
-      }else{
-        form.reset();
-        setTimeout(()=>{
-          statusTxt.style.display = "none";
-        }, 3000);
-      }
-      statusTxt.innerText = response;
-      form.classList.remove("disabled");
-    }
-  }
-  let formData = new FormData(form);
-  xhr.send(formData);
+const form = document.querySelector("form");
+const overallFeedback = document.querySelector('#overallFeedback')
+overallFeedback.innerHTML = "";
+form.onsubmit = () => {
+overallFeedback.innerHTML = "Message sent!";
+overallFeedback.style.color = "lightgreen"
 }
